1826367d45ee18f67a88161a1771080f618a7b52
zoeldyik <zoeldyik@gmail.com>
2022-10-20 09:26:25 +0700

    add pwa to web

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['bg.svg', 'error.svg', 'icon-192x192.png', 'icon-512x512.png'],
+      manifest: {
+        name: 'Baca Quran',
+        short_name: 'Baca Quran',
+        theme_color: "#fafafa",
+        background_color:"#fafafa",
+        display:"standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
